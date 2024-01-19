d11ee321fdc271a58bf354089e31b83b456ea9d5
Macbook <macbook@Vako-MacBook-Air.local>
2024-01-20 00:54:16 +0400

    first commit

---
+++ src/main/java/az/edu/turing/lesson04.java
@@ -0,0 +1,18 @@
+package az.edu.turing;
+
+
+public class lesson04 {
+    public static void main(String[] args) {
+
+
+        byte b1 = 4;
+        byte b2 = (byte)129;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        int i=1;
+        byte b4=(byte)i;
+        System.out.println(b4);
+
+    }
+}
