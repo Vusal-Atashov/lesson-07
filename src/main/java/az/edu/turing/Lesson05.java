d490e2a4ca7d3bad54cd73755cc1e662d0d14202
macbook <vusalatashov@gmail.com>
2024-01-22 19:38:23 +0400

    init Lesson06.java

--- src/main/java/az/edu/turing/Lesson05.java
+++ src/main/java/az/edu/turing/Lesson05.java
@@ -6,7 +6,6 @@ public class Lesson05 {
     public static void main(String[] args) {
         Scanner number = new Scanner(System.in);
         int a = number.nextInt();
-
         System.out.println(a > 0 & a <= 3 ? "Spring" : a > 3 & a <= 6 ? "Summer" : a > 6 & a <= 9 ? "Autumn" : a > 9 & a <= 12 ? "winter" : "false");
     }
 }
