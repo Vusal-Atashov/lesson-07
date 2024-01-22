955f63f92f4988c1c0cfd6f84f98f8aa48b36e8b
macbook <vusalatashov@gmail.com>
2024-01-22 19:26:33 +0400

    init lesson04.java

--- src/main/java/az/edu/turing/lesson04.java
+++ src/main/java/az/edu/turing/lesson04.java
@@ -4,10 +4,9 @@ import java.util.Scanner;
 
 public class lesson04 {
     public static void main(String[] args) {
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.print("Word :");
-        String word=scan.nextLine();
-        System.out.println(2*word.length());
-
+        String word = scan.nextLine();
+        System.out.println(2 * word.length());
     }
 }
