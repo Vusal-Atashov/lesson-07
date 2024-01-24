2bf9b00fdca12a5db81025a5a9449d1d7c59d7cc
macbook <vusalatashov@gmail.com>
2024-01-24 20:51:30 +0400

    init DiviserApp.java

--- src/main/java/az/edu/turing/lesson07/ArrayApp.java
+++ src/main/java/az/edu/turing/lesson07/ArrayApp.java
@@ -11,35 +11,35 @@ public class ArrayApp {
         int prod = 1;
         int oddIndexSum = 0;
         int evenIndexProd = 1;
-
         int[] numArray = new int[arraySize];
+
         for (int i = 0; i < arraySize; i++) {
             numArray[i] = scanner.nextInt();
             if (i % 2 != 0) {
                 oddIndexSum += numArray[i];
             } else evenIndexProd *= numArray[i];
         }
-        int max=numArray[0];
-        int min=numArray[0];
+        int max = numArray[0];
+        int min = numArray[0];
 
         for (int element : numArray) {
             System.out.print(element * 2 + " ");
             sum += element;
             prod *= element;
 
-            if(max<element){
-                max=element;
+            if (max < element) {
+                max = element;
             }
-            if (min>element){
-                min=element;
+            if (min > element) {
+                min = element;
             }
         }
         System.out.println("\nSum: " + sum);
         System.out.println("Prod: " + prod);
         System.out.println("Odd index sum: " + oddIndexSum);
         System.out.println("Even index prod: " + evenIndexProd);
-        System.out.println("Max: "+max);
-        System.out.println("Min: "+min);
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
         System.out.println(Arrays.toString(numArray));
     }
 }
