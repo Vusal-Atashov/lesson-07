5a429e8bef7f6b9586fc9a8ed2dc1afd2c3197ab
macbook <vusalatashov@gmail.com>
2024-01-24 19:15:10 +0400

    init DiviserApp.java

--- src/main/java/az/edu/turing/lesson07/DiviserApp.java
+++ src/main/java/az/edu/turing/lesson07/DiviserApp.java
@@ -2,12 +2,12 @@ package az.edu.turing.lesson07;
 
 import java.util.Scanner;
 
-public class Task1App {
+public class DiviserApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        for (int i = 1; i <=a; i++) {
-            if (a % i == 0) {
+        int number = scanner.nextInt();
+        for (int i = 1; i <=number; i++) {
+            if (number % i == 0) {
                 System.out.print(i + " ");
             }
         }
