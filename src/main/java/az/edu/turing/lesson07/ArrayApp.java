0f29e87bda5cbf39391c307a8b392ab3d5f1d7b4
macbook <vusalatashov@gmail.com>
2024-01-24 20:37:14 +0400

    init DiviserApp.java

--- src/main/java/az/edu/turing/lesson07/ArrayApp.java
+++ src/main/java/az/edu/turing/lesson07/ArrayApp.java
@@ -7,19 +7,19 @@ public class ArrayApp {
     public static void main(String[] args) {
         int arraySize = new Scanner(System.in).nextInt();
         Scanner scanner = new Scanner(System.in);
-        int sum =0;
-        int prod=1;
+        int sum = 0;
+        int prod = 1;
         int[] numArray = new int[arraySize];
         for (int i = 0; i < arraySize; i++) {
             numArray[i] = scanner.nextInt();
         }
         for (int element : numArray) {
             System.out.print(element * 2 + " ");
-            sum+=element;
-            prod*=element;
+            sum += element;
+            prod *= element;
         }
-        System.out.println("\nSum: "+sum);
-        System.out.println("Prod: "+prod);
+        System.out.println("\nSum: " + sum);
+        System.out.println("Prod: " + prod);
         System.out.println(Arrays.toString(numArray));
     }
 }
