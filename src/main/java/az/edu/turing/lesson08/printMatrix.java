7c9c6433943211be694501a7d4c787e63d970cb6
macbook <vusalatashov@gmail.com>
2024-01-26 20:15:52 +0400

    init array-2d

--- src/main/java/az/edu/turing/lesson08/printMatrix.java
+++ src/main/java/az/edu/turing/lesson08/printMatrix.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class printMatrix {
     public static void main(String[] args) {
         int number = new Scanner(System.in).nextInt();
-        int[][] array2D = new int[number][number];
+        int[][] arrayTwoDimensional = new int[number][number];
         for (int i = 0; i < number; i++) {
             for (int j = 0; j < number; j++) {
                 array2D[i][j] = number;
