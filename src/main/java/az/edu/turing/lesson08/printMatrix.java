4dfe996a85035c5776cbcb48ae6b1b3efa0a31eb
macbook <vusalatashov@gmail.com>
2024-01-29 02:26:26 +0400

    array-2d

--- src/main/java/az/edu/turing/lesson08/printMatrix.java
+++ src/main/java/az/edu/turing/lesson08/printMatrix.java
@@ -9,9 +9,44 @@ public class printMatrix {
         for (int i = 0; i < number; i++) {
             for (int j = 0; j < number; j++) {
                 arrayTwoDimensional[i][j] = number;
-                System.out.print(arrayTwoDimensional[i][j]);
+                System.out.print(arrayTwoDimensional[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        int j;
+        for (int i = number; i > 0; i--) {
+            for (j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            for (int s = 0; s < number - i; s++) {
+                System.out.print(" ");
+                System.out.print(" ");
+
+            }
+            for (j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            if (i > 1) {
+                System.out.println();
+            }
+
+        }
+        for (int i = 0; i <= number; i++) {
+
+            for (j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int s = 0; s < number - i; s++) {
+                System.out.print(" ");
+                System.out.print(" ");
+
+            }
+            for (j = 0; j < i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
     }
 }
+
