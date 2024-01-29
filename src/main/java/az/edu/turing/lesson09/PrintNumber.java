7bfdd0165d502f5cabb12f20f07ebdd91c76233a
macbook <vusalatashov@gmail.com>
2024-01-29 19:18:31 +0400

    trueFalse

---
+++ src/main/java/az/edu/turing/lesson09/PrintNumber.java
@@ -0,0 +1,9 @@
+package az.edu.turing.lesson09;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        int num =5;
+        if(num%2!=0) System.out.println(true);
+        else System.out.println(false);
+    }
+}
