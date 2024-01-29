da1c3d6ba1dfe06d7216f54c4175492651147d5c
macbook <vusalatashov@gmail.com>
2024-01-29 19:19:58 +0400

    trueFalse

--- src/main/java/az/edu/turing/lesson09/PrintNumber.java
+++ src/main/java/az/edu/turing/lesson09/PrintNumber.java
@@ -2,8 +2,8 @@ package az.edu.turing.lesson09;
 
 public class PrintNumber {
     public static void main(String[] args) {
-        int num =5;
-        if(num%2!=0) System.out.println(true);
+        int num = 5;
+        if (num % 2 != 0) System.out.println(true);
         else System.out.println(false);
     }
 }
