0bbe4141a9d1016106253b78bf5900a9a58f9446
macbook <vusalatashov@gmail.com>
2024-01-29 20:46:32 +0400

    print arr

--- src/main/java/az/edu/turing/lesson09/PrintArray.java
+++ src/main/java/az/edu/turing/lesson09/PrintArray.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 
 public class PrintArray {
     public static void main(String[] args) {
-        int[] arr = new int[ScannerHelper()];
+        int[] arr = new int[scannerHelper()];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < arr.length; j++) {
                 arr[j] = (int) (Math.random() * 100);
             }
-            PrintArr(arr);
+            printArr(arr);
         }
     }
 
-    static int ScannerHelper() {
+    static int scannerHelper() {
         return new Scanner(System.in).nextInt();
     }
 
-    static void PrintArr(int[] a) {
+    static void printArr(int[] a) {
         for (int i = 0; i < a.length; i++) {
             System.out.println(i + " -> " + a[i]);
         }
