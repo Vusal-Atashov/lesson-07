9441be674eef756f613bd88aa4146c4df34e76b2
macbook <vusalatashov@gmail.com>
2024-01-31 19:57:21 +0400

    Lesson10

---
+++ src/main/java/az/edu/turing/lesson10/Metods.java
@@ -0,0 +1,19 @@
+package az.edu.turing.lesson10;
+
+import java.util.Scanner;
+
+public class Metods {
+    public static void main(String[] args) {
+
+        intPrint();
+        doublePrint();
+    }
+
+    public static void intPrint() {
+        System.out.println(new Scanner(System.in).nextInt() + 2);
+    }
+
+    public static void doublePrint(double a) {
+        System.out.println(new Scanner(System.in).nextDouble() + 2);
+    }
+}
