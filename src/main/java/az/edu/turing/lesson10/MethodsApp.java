453a82f7b1624ee69c5d7e8b83c7c04003292433
macbook <vusalatashov@gmail.com>
2024-01-31 20:06:49 +0400

    Lesson10

---
+++ src/main/java/az/edu/turing/lesson10/MethodsApp.java
@@ -0,0 +1,20 @@
+package az.edu.turing.lesson10;
+
+import java.util.Scanner;
+
+public class MethodsApp {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+        double b = new Scanner(System.in).nextDouble();
+        intPrint(a);
+        doublePrint(b);
+    }
+
+    public static void intPrint(int a) {
+        System.out.println(a + 2);
+    }
+
+    public static void doublePrint(double a) {
+        System.out.println(a + 2);
+    }
+}
