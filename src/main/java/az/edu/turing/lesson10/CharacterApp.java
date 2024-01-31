0d844c225032ed3034f7af34f2e766e1b5e0db84
macbook <vusalatashov@gmail.com>
2024-01-31 20:31:39 +0400

    Lesson10

---
+++ src/main/java/az/edu/turing/lesson10/CharacterApp.java
@@ -0,0 +1,14 @@
+package az.edu.turing.lesson10;
+
+public class CharacterApp {
+    public static void main(String[] args) {
+        char [] arr={'J','A','V','A',' ','B','A','C','K','E','N','D'};
+        print(arr);
+
+    }
+    public static void print(char[] word){
+        for (char c : word) {
+            System.out.print(c);
+        }
+    }
+}
