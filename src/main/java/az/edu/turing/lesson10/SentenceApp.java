8db962cd220cd621ae965607ab517b0395302f35
macbook <vusalatashov@gmail.com>
2024-02-02 19:04:14 +0400

    Lesson10

---
+++ src/main/java/az/edu/turing/lesson10/SentenceApp.java
@@ -0,0 +1,20 @@
+package az.edu.turing.lesson10;
+
+import java.util.Scanner;
+
+public class SentenceApp {
+    public static void main(String[] args) {
+        String sentence1=new Scanner(System.in).nextLine();
+        String sentence2=new Scanner(System.in).nextLine();
+
+        print(sentence2.substring(5));
+        print(sentence2.trim());
+        print(sentence2.toLowerCase());
+        print(sentence2.concat(sentence1));
+        print(sentence2.);
+
+    }
+    public static void print(String a){
+        System.out.println(a);
+    }
+}
