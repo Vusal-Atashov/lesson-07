7a9527f0b2a63808a61f213aadc45e038e67acb5
macbook <vusalatashov@gmail.com>
2024-02-02 19:54:27 +0400

    Lesson11

--- src/main/java/az/edu/turing/lesson11/StringMethod.java
+++ src/main/java/az/edu/turing/lesson11/StringMethod.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class StringMethod {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String email=scanner.nextLine();
+        String email = scanner.nextLine();
         System.out.println(nameCut(email));
         nameLength(nameCut(email));
     }
-    public static String nameCut(String email){
+
+    public static String nameCut(String email) {
         return email.substring(0, email.indexOf("@"));
     }
-    public static void nameLength(String name){
+
+    public static void nameLength(String name) {
         System.out.println(name.length());
     }
 
