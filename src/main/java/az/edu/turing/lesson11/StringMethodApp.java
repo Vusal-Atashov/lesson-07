cf56342adadcea83fc07b03e51091043827d2599
macbook <vusalatashov@gmail.com>
2024-02-02 20:19:57 +0400

    EmailApp

--- src/main/java/az/edu/turing/lesson11/StringMethodApp.java
+++ src/main/java/az/edu/turing/lesson11/StringMethodApp.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class StringMethodApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String email = scanner.nextLine();
-        String name = nameCut(email);
-        nameLength(name);
+        final String email = scanner.next();
+        final String name = nameCut(email);
+        System.out.println(name);
+        System.out.println(nameLength(name));
     }
 
     public static String nameCut(String email) {
         return email.substring(0, email.indexOf("@"));
     }
 
-    public static void nameLength(String name) {
-        System.out.println(name);
-        System.out.println(name.length());
+    public static int nameLength(String name) {
+        return name.length();
     }
 }
