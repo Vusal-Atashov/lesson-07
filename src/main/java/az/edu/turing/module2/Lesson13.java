20c5d379d68a0ec1190f02c5232370c7c713a0c6
macbook <vusalatashov@gmail.com>
2024-02-12 20:19:58 +0400

    JavaBackend init

--- src/main/java/az/edu/turing/module2/Lesson13.java
+++ src/main/java/az/edu/turing/module2/Lesson13.java
@@ -2,6 +2,19 @@ package az.edu.turing.module2;
 
 public class Lesson13 {
     public static void main(String[] args) {
-
+        String [] names=new String[]{"Vusal","Ferid","Osman","Ismayil"};
+        int []age=new int[]{21,22,24,28};
+        double[]grade=new double[]{82.5,73.7,93.0,88.9};
+        int index=maxGradeIndex(grade);
+        System.out.println(" name : "+names[index]+" | age : "+age[index]+" | grade : "+grade[index]);
+    }
+    public static int maxGradeIndex(double[] grade){
+        int maxIndex=0;
+        for (int i = 0; i < grade.length; i++) {
+            if (grade[maxIndex]<grade[i]){
+                maxIndex=i;
+            }
+        }
+        return maxIndex;
     }
 }
