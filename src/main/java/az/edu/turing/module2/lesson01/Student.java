ad34a561e9cf8e12dd2c30a217fe9049a0d860f8
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-02-14 15:21:48 +0400

    StudentApp init

---
+++ src/main/java/az/edu/turing/module2/lesson01/Student.java
@@ -0,0 +1,39 @@
+package az.edu.turing.module2.lesson01;
+
+public class Student {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Double grade) {
+        this.grade = grade;
+    }
+
+    int age;
+    Double grade;
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+}
--- src/main/java/az/edu/turing/module2/lesson01/StudentApp.java
+++ src/main/java/az/edu/turing/module2/lesson01/StudentApp.java
@@ -1,4 +1,31 @@
 package az.edu.turing.module2.lesson01;
 
+import java.util.Arrays;
+
 public class StudentApp {
+    public static void main(String[] args) {
+        Student student1 = new Student("vusal", 21, 71);
+        Student student2 = new Student("ferid", 23, 65);
+        Student student3 = new Student("Ismayil", 28, 86);
+        Student[] students = new Student[3];
+        students[0] = student1;
+        students[1] = student2;
+        students[2] = student3;
+
+        Student maxGrade = maxGradeStudent(students);
+
+        System.out.println(" | " + maxGrade.name + " | " + maxGrade.age + " | " + maxGrade.grade + " | ");
+    }
+
+    public static Student maxGradeStudent(Student[] students) {
+        double maxGarde = students[0].grade;
+        int j = 0;
+        for (int i = 0; i < students.length; i++) {
+            if (maxGarde < students[i].grade) {
+                maxGarde = students[i].grade;
+                j = i;
+            }
+        }
+        return students[j];
+    }
 }
