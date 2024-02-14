cc063295d32827601ab39ca7667f416844cb8092
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-02-14 15:27:52 +0400

    StudentApp

--- src/main/java/az/edu/turing/module2/lesson01/StudentApp.java
+++ src/main/java/az/edu/turing/module2/lesson01/StudentApp.java
@@ -1,16 +1,18 @@
 package az.edu.turing.module2.lesson01;
 
 
-
 public class StudentApp {
     public static void main(String[] args) {
         Student student1 = new Student("vusal", 21, 71);
         Student student2 = new Student("ferid", 23, 65);
         Student student3 = new Student("Ismayil", 28, 86);
+
         Student[] students = new Student[3];
+
         students[0] = student1;
         students[1] = student2;
         students[2] = student3;
+
         student1.setGrade(99.99);
 
         Student maxGrade = maxGradeStudent(students);
