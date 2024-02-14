6be515aa99213a69e8b39747b18aa0306f694489
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-02-14 14:47:57 +0400

    JavaBackend init

--- src/main/java/az/edu/turing/module2/Lesson13.java
+++ src/main/java/az/edu/turing/module2/Lesson13.java
@@ -2,17 +2,18 @@ package az.edu.turing.module2;
 
 public class Lesson13 {
     public static void main(String[] args) {
-        String [] names=new String[]{"Vusal","Ferid","Osman","Ismayil"};
-        int []age=new int[]{21,22,24,28};
-        double[]grade=new double[]{82.5,73.7,93.0,88.9};
-        int index=maxGradeIndex(grade);
-        System.out.println(" name : "+names[index]+" | age : "+age[index]+" | grade : "+grade[index]);
+        String[] names = new String[]{"Vusal", "Ferid", "Osman", "Ismayil"};
+        int[] ages = new int[]{21, 22, 24, 28};
+        double[] grades = new double[]{82.5, 73.7, 93.0, 88.9};
+        int index = maxGradeIndex(grades);
+        System.out.println(" name : " + names[index] + " | age : " + ages[index] + " | grade : " + grades[index]);
     }
-    public static int maxGradeIndex(double[] grade){
-        int maxIndex=0;
-        for (int i = 0; i < grade.length; i++) {
-            if (grade[maxIndex]<grade[i]){
-                maxIndex=i;
+
+    public static int maxGradeIndex(double[] grades) {
+        int maxIndex = 0;
+        for (int i = 0; i < grades.length; i++) {
+            if (grades[maxIndex] < grades[i]) {
+                maxIndex = i;
             }
         }
         return maxIndex;
