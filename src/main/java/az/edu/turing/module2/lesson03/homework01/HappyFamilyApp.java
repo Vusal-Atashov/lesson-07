3ae948f4f82f6ce4241827c9c2a68fb26a010591
Vusal Atashov <156101309+Vusal-Atashov@users.noreply.github.com>
2024-02-21 15:05:52 +0400

    Human

--- src/main/java/az/edu/turing/module2/lesson03/homework01/HappyFamilyApp.java
+++ src/main/java/az/edu/turing/module2/lesson03/homework01/HappyFamilyApp.java
@@ -3,14 +3,5 @@ package az.edu.turing.module2.lesson03.homework01;
 public class HappyFamilyApp {
     public static void main(String[] args) {
 
-        Human human1=new Human("vusal",
-                "Atashov",
-                2002,
-                85,
-                new Pet("cat","Lucy",2,4,new String[4]),
-                new Human("Sevinc","Atashova",1974),
-                new Human("Arif","Atashov",1960),
-                new String[1][1]);
-        System.out.println(human1.toString());
     }
 }
